57877247a76f2507f837fa7619b72d29218fe255
nemoxiaolan <335454250@qq.com>
2014-03-17 15:39:07 +0800

    更新数据库结构

--- erp_server/Lib/Action/JXC/GoodsAction.class.php
+++ erp_server/Lib/Action/JXC/GoodsAction.class.php
@@ -13,46 +13,9 @@
 class GoodsAction extends CommonAction {
     
     public function index() {
-        
         $goodsModel = D("GoodsCatView");
-        
-        $count = $goodsModel->count();
-        list($limit, $totalPage) = $this->getQueryLimit($count, $_GET["page"] ? abs(intval($_GET["page"])) : 1, 25);
         $theGoods = $goodsModel->select();
         $this->response($theGoods);
-    }
-    
-//    public function index() {
-//        //列表过滤器，生成查询Map对象
-//        $map = $this->_search();
-//        if (method_exists($this, '_filter')) {
-//            $this->_filter($map);
-//        }
-//        $model = D("GoodsCatView");
-//        if (!empty($model)) {
-//            $this->_list($model, $map);
-//        }
-//        cookie('_currentUrl_', __SELF__);
-//        $this->display();
-//    }
-    
-    private function _assign_categories() {
-        $cat = D("GoodsCategory");
-        $data = $cat->getTree(1);
-        foreach($data as $k=>$t) {
-            $data[$k]["prefix_name"] = $t["prefix"].$t["name"];
-        }
-        $data = $cat->getIndexArray($data, "prefix_name");
-        $this->assign("theCategories", $data);
-    }
-    
-    
-    public function _before_add() {
-        $this->_assign_categories();
-    }
-    
-    public function _before_edit() {
-        $this->_assign_categories();
     }
     
 }
