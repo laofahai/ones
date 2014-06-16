d1f396de19c24e8ee6d824121c11a838d6ada3de
nemoxiaolan <335454250@qq.com>
2014-06-17 00:23:19 +0800

    修复执行生产工艺时工艺顺序错误问题，增加进入生产工序后ProducePlanDetail.status = 1标识已开始生产

--- erp_server/Lib/Action/Statistics/ProductViewAction.class.php
+++ erp_server/Lib/Action/Statistics/ProductViewAction.class.php
@@ -6,9 +6,41 @@
  * Time: 14:47
  */
 
-namespace Statistics;
 
+class ProductViewAction extends CommonAction{
 
-class ProductViewAction {
+    /*
+     * @todo filter
+     * 销售，采购，生产
+     * */
+    public function index() {
+
+        $dateStart = strtotime(date("Y-m"));
+        $dateEnd   = CTS;
+
+        $voList = array(
+
+        );
+
+        $baseMap = array(
+            "dateline" => array("BETWEEN", array($dateStart, $dateEnd)),
+            "status"   => array("GT", 1)
+        );
+
+        $orderModel = D("Orders");
+        $theOrders = $orderModel->field("total_num, total_amount_real")
+            ->where($baseMap)->select();
+        foreach($theOrders as $o) {
+            $voList["orders_num"] += $o["total_num"];
+            $voList["orders_amount"] += $o["total_amount_real"];
+        }
+
+        $purchaseModel = D("Purchase");
+        $thePurchases = $purchaseModel->field("quantity,total_price_real")->where($baseMap)->select();
+        foreach($thePurchases as $p) {
+            $voList["purchase_num"] += $p["total_num"];
+            $voList["purchase_"] += $p["total_amount_real"];
+        }
+    }
 
 } 
