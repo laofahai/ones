9cd7d61fa1efd0ae7906813154d3edc2b7193e0b
nemoxiaolan <335454250@qq.com>
2014-06-16 23:38:29 +0800

    unamed

---
+++ app/scripts/Statistics/service.js
@@ -0,0 +1,3 @@
+/**
+ * Created by nemo on 6/16/14.
+ */
---
+++ erp_server/Lib/Action/Statistics/ProductViewAction.class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nemo
+ * Date: 6/16/14
+ * Time: 14:47
+ */
+
+namespace Statistics;
+
+
+class ProductViewAction {
+
+} 
