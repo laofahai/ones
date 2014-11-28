889149e69bbad98dd6a04eb46e190884b04bb944
nemoxiaolan <335454250@qq.com>
2014-11-28 21:56:27 +0800

    优化：UI小变动

--- apps/remind/backend/Behavior/ONESRemindHTMLBehavior.class.php
+++ apps/remind/backend/Behavior/ONESRemindHTMLBehavior.class.php
@@ -11,13 +11,13 @@ class ONESRemindHTMLBehavior extends Behavior {
 
     public function run(&$params) {
         echo <<<EOF
-        <li class="purple" ng-controller="NavRemindCtl">
+        <li ng-controller="NavRemindCtl">
             <a data-toggle="dropdown" class="dropdown-toggle">
                 <i class="icon icon-bell" ng-class="{'icon-animated-bell': reminds.length>0}"></i>
                 <span class="badge badge-important" ng-bind="reminds.length" ng-show="reminds.length>0"></span>
             </a>
 
-            <ul class="pull-right dropdown-navbar navbar-pink dropdown-menu dropdown-caret dropdown-close header-reminds-list">
+            <ul class="pull-right dropdown-navbar dropdown-menu dropdown-caret dropdown-close header-reminds-list">
                 <li ng-repeat="rem in reminds">
                     <span class="pull-left" template="rem.content" data-id="{{rem.id}}" compile-data></span>
                     <label class="pull-right badge badge-info">+{{rem.num}}</label>
